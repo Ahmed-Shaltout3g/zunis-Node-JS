4c315c5b964f991a64607e350d73a80efe035c58
Ahmed-Shaltout3g <120695032+Ahmed-Shaltout3g@users.noreply.github.com>
2024-01-26 00:27:04 +0200

    3

--- DB/connection.js
+++ DB/connection.js
@@ -4,8 +4,6 @@ export const connectionDB = async () => {
   return await mongoose
     .connect(process.env.DB_CLOUD)
     .then(() => {
-      console.log(process.env.DB_CLOUD);
-
       console.log("connection DB success");
     })
     .catch(() => {
