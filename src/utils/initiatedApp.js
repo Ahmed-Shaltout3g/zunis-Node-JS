7177845c0ac5dd0d37b8ad74adaba1a12736fe9f
Ahmed-Shaltout3g <120695032+Ahmed-Shaltout3g@users.noreply.github.com>
2024-01-26 01:33:56 +0200

    q

--- src/utils/initiatedApp.js
+++ src/utils/initiatedApp.js
@@ -5,7 +5,10 @@ import cors from "cors";
 import morgan from "morgan";
 export const initatApp = (express, app) => {
   const port = process.env.PORT || 5000;
-  var whitelist = ["http://example1.com", "http://127.0.0.1:5500"];
+  var whitelist = [
+    "https://zunis-node-1dspyqtxq-ahmed-shaltouts-projects.vercel.app",
+    "http://127.0.0.1:5500",
+  ];
   app.use(express.json());
   if (process.env.ENV_MODE == "DEV") {
     app.use(cors());
