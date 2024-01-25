9af4042b47862fe27d7da5c9f3c3334a94d9905a
Ahmed-Shaltout3g <120695032+Ahmed-Shaltout3g@users.noreply.github.com>
2024-01-26 01:26:41 +0200

    2

--- src/utils/initiatedApp.js
+++ src/utils/initiatedApp.js
@@ -2,12 +2,29 @@ import { connectionDB } from "../../DB/connection.js";
 import * as allRoutes from "../Modules/index.routes.js";
 import { globalResponse } from "./errorHandling.js";
 import cors from "cors";
+import morgan from "morgan";
 export const initatApp = (express, app) => {
   const port = process.env.PORT || 5000;
 
   app.use(express.json());
+  if (process.env.ENV_MODE == "DEV") {
+    app.use(cors());
+    app.use(morgan("dev"));
+  } else {
+    app.use(async (req, res, next) => {
+      if (!whitelist.includes(req.header("origin"))) {
+        return next(new Error("Not allowed by CORS", { cause: 502 }));
+      }
+      await res.header("Access-Control-Allow-Origin", "*");
+      await res.header("Access-Control-Allow-Header", "*");
+      await res.header("Access-Control-Allow-Private-Network", "true");
+      await res.header("Access-Control-Allow-Method", "*");
+      next();
+    });
+    app.use(morgan("combined"));
+  }
   connectionDB();
-  app.use(cors());
+
   app.use("/category", allRoutes.catagoryRoutes);
   app.use("/auth", allRoutes.authRoutes);
   app.use("/product", allRoutes.productRoutes);
