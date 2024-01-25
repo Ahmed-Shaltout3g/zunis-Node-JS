9770a584521b5fbff3c4f4acd0d92a1d06534b12
Ahmed-Shaltout3g <120695032+Ahmed-Shaltout3g@users.noreply.github.com>
2024-01-26 01:35:27 +0200

    1

--- src/utils/initiatedApp.js
+++ src/utils/initiatedApp.js
@@ -2,30 +2,10 @@ import { connectionDB } from "../../DB/connection.js";
 import * as allRoutes from "../Modules/index.routes.js";
 import { globalResponse } from "./errorHandling.js";
 import cors from "cors";
-import morgan from "morgan";
 export const initatApp = (express, app) => {
   const port = process.env.PORT || 5000;
-  var whitelist = [
-    "https://zunis-node-1dspyqtxq-ahmed-shaltouts-projects.vercel.app",
-    "http://127.0.0.1:5500",
-  ];
+  app.use(cors());
   app.use(express.json());
-  if (process.env.ENV_MODE == "DEV") {
-    app.use(cors());
-    app.use(morgan("dev"));
-  } else {
-    app.use(async (req, res, next) => {
-      if (!whitelist.includes(req.header("origin"))) {
-        return next(new Error("Not allowed by CORS", { cause: 502 }));
-      }
-      await res.header("Access-Control-Allow-Origin", "*");
-      await res.header("Access-Control-Allow-Header", "*");
-      await res.header("Access-Control-Allow-Private-Network", "true");
-      await res.header("Access-Control-Allow-Method", "*");
-      next();
-    });
-    app.use(morgan("combined"));
-  }
   connectionDB();
 
   app.use("/category", allRoutes.catagoryRoutes);
