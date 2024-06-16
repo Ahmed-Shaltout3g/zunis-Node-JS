8e289aa225744ad46acfe111703a05926d46803d
Ahmed-Shaltout3g <120695032+Ahmed-Shaltout3g@users.noreply.github.com>
2024-06-17 02:28:19 +0300

    message update

--- src/Modules/Message/message.controllar.js
+++ src/Modules/Message/message.controllar.js
@@ -48,7 +48,7 @@ export const getAllMessagesToAdmin = async (req, res, next) => {
 
 export const getAllMessagesToUser = async (req, res, next) => {
   const { id } = req.params;
-  const messages = await messageModel.findOne({ sentTo: id });
+  const messages = await messageModel.find({ sentTo: id });
   res.status(200).json({
     success: true,
     messages,
